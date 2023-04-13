543a529cc05937248b97bb7319ccd56e9595ca8b
Phrank Lenoci <fcl1118@live.com>
2023-04-12 23:09:58 -0500

    small updates

--- src/API/cta.js
+++ src/API/cta.js
@@ -1,21 +1,30 @@
 import axios from "axios";
 
+var northBoundStopID = 17266;
+var southBoundStopID = 1837;
+
 var baseUrl =
   "http://ctabustracker.com/bustime/api/v3/getpredictions?key=" +
   process.env.REACT_APP_CTA_KEY +
-  "&rt=22&stpid=17266&format=json&top=5";
+  "&format=json&top=5";
 
-export const getBusTime = async () => {
+export const getBusTime = async (routeNumber, direction) => {
   try {
+    var directionID;
+    if (direction === "North") {
+      directionID = northBoundStopID;
+    } else {
+      directionID = southBoundStopID;
+    }
     var config = {
       method: "get",
-      url: baseUrl,
+      url: baseUrl + "&rt=" + routeNumber + "&stpid=" + directionID,
       headers: {},
     };
 
     return await axios(config)
       .then(function (response) {
-        return JSON.stringify(response.data["bustime-response"].prd[0].dstp);
+        return JSON.stringify(response.data["bustime-response"].prd[0].prdctdn);
       })
       .catch(function (error) {
         console.log(error);
@@ -23,4 +32,6 @@ export const getBusTime = async () => {
   } catch (e) {
     console.log(e);
   }
+
+  const getDirec = async () => {};
 };
--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -1,25 +1,48 @@
 import { React, useState, useEffect } from "react";
 import { getBusTime } from "../../API/cta";
+import { Button } from "@mui/material";
 
 export default function Tracker() {
-  const [distance, setDistance] = useState();
   const [refreshInterval, setRefreshInterval] = useState(200 || 0);
-
+  const [route1, setRoute1] = useState(22);
+  const [distance1, setDistanceRoute1] = useState();
+  const [route2, setRoute2] = useState(36);
+  const [distance2, setDistanceRoute2] = useState();
+  const [direction, setDirection] = useState("North");
   const fetchMetrics = () => {
-    getBusTime().then((res) => {
-      setDistance(res);
+    getBusTime(route1, direction).then((res) => {
+      setDistanceRoute1(res);
+    });
+    getBusTime(route2, direction).then((res) => {
+      setDistanceRoute2(res);
     });
   };
-
+  const changeDirection = () => {
+    if (direction === "North") {
+      setDirection("South");
+      fetchMetrics();
+    } else {
+      setDirection("North");
+      fetchMetrics();
+    }
+  };
   useEffect(() => {
     if (refreshInterval && refreshInterval > 0) {
       const interval = setInterval(fetchMetrics, refreshInterval);
       return () => clearInterval(interval);
     }
   }, [refreshInterval]);
+
   return (
-    <h1>
-      North Bound 22 Bus at Clark and Arlington will be here in {distance} feet{" "}
-    </h1>
+    <div>
+      <Button size="large" variant="contained" onClick={changeDirection}>
+        Change Direction
+      </Button>
+      <h1>
+        {direction} Bound {route1} Bus at Clark and Arlington will be here in{" "}
+        {distance1} minutes <br></br> {direction} Bound {route2} Bus at Clark
+        and Arlington will be here in {distance2} minutes{" "}
+      </h1>
+    </div>
   );
 }
